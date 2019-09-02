9edcea29bd1e47c2624f84475e8daee5b917ce25
Ray Low <raychunyin00@gmail.com>
2019-09-02 12:45:45 +0800

    Cleaned up comments

---
+++ app1/forms.py
@@ -0,0 +1,48 @@
+from django.forms import ModelForm, Textarea, DateInput, TextInput, IntegerField, SelectMultiple, ChoiceField, EmailField, CharField
+from django.forms import SelectDateWidget
+from app1.models import User, Event, Registration
+from bootstrap_datepicker_plus import DatePickerInput
+from django.contrib.auth.forms import UserCreationForm
+
+'''class UserCreateForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+        widgets = {
+            'username': TextInput(attrs={'class':'input', 'type':'text', 'placeholder':'Enter a username'}),
+            'password1': TextInput(attrs={'class':'input', 'type':'password', 'placeholder':'Enter a password'}),
+            'password2': TextInput(attrs={'class':'input', 'type':'password', 'placeholder':'Re-enter the password'}),
+        }
+
+    def save(self, commit=True):
+        user = super(UserCreateForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.IC = self.cleaned_data['IC_number']
+        if commit:
+            user.save()
+        return user
+'''
+
+class RegistrationForm(ModelForm):
+    class Meta:
+        model = Registration
+        fields = ['date']
+        widgets = {
+            'user': TextInput(attrs={'class':'input', 'type':'text', 'placeholder':'Enter your name'}),
+            'date': DatePickerInput(format='%Y-%m-%d'),
+            #'pin': TextInput(attrs={'class':'input', 'type':'text', 'placeholder':'PIN number'}),
+        }
+
+
+class addEventForm(ModelForm):
+    class Meta:
+        model = Event
+        fields = ['name', 'location', 'address', 'details_days', 'details_time', 'volunteers', 'pin']
+        widgets = {
+            'name' : TextInput(attrs={'class':'input', 'type':'text', 'placeholder':'Enter a name for the event'}),
+            'location': TextInput(attrs={'class':'input','type':'text','placeholder':'Enter a short name for the location'}),
+            'address': TextInput(attrs={'class':'input','type':'text','placeholder':'Enter the full address for the location'}),
+            'details_time': TextInput(attrs={'class':'input','type':'text','placeholder':'E.g. 2pm - 5pm on weekdays, 4pm-6pm on weekends'}),
+            'volunteers': TextInput(attrs={'class':'input', 'type':'text', 'placeholder':'Enter the number of volunteers needed : 10'}),
+            'pin': TextInput(attrs={'class':'input', 'type':'text', 'placeholder':'Enter the confirmation PIN number'}),
+        }
---
+++ app1/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser, BaseUserManager
+from django.contrib.postgres.fields import ArrayField
+from django.utils import timezone
+from multiselectfield import MultiSelectField
+# Create your models here.
+
+CHOICES = (
+    ('Monday', 'Monday'),
+    ('Tuesday', 'Tuesday'),
+    ('Wednesday', 'Wednesday'),
+    ('Thursday', 'Thursday'),
+    ('Friday', 'Friday'),
+    ('Saturday', 'Saturday'),
+    ('Sunday', 'Sunday'),
+)
+
+
+'''class UserManager(BaseUserManager):
+    def createUser(self, username, password, email, IC_number):
+        if not email and IC_number:
+            raise ValueError('Users must have an email and IC number')
+        user = self.model(
+            username=username,
+            email = self.normalize_email(email),
+            IC_number = IC_number
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def createSuperuser(self, username, password, email, IC_number):
+        user = self.createUser(username, email, password, IC_number)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+'''
+
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    password1 = models.CharField(max_length=20, blank=False, null=False)
+    password2 = models.CharField(max_length=20, blank=False, null=False)
+    email = models.EmailField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.TextField()
+    address = models.TextField()
+    details_days = MultiSelectField(max_length=100, max_choices=7, choices=CHOICES)
+    details_time = models.TextField()
+    volunteers = models.IntegerField()
+    pin = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Registration(models.Model):
+    user = models.CharField(max_length=100)
+    date = models.DateField(default=timezone.now, blank=True, null=True)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
