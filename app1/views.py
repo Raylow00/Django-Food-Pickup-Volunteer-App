c87ad0254e8b50b5134a07e69978486230400396
Ray Low <44168708+Raylow00@users.noreply.github.com>
2019-09-04 02:00:44 +0800

    Add files via upload

--- app1/views.py
+++ app1/views.py
@@ -5,14 +5,10 @@ from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.http import require_POST
 from .forms import RegistrationForm, addEventForm
 from .models import User, Event, Registration
-<<<<<<< HEAD
-from datetime import date
-from django.contrib import messages
 
-=======
+
 
 # Create your views here.
->>>>>>> 1e528b7bb12039c904c40d5f9d8a2543b9d79912
 def signup(request):
 
     if request.method == 'POST':
@@ -22,10 +18,6 @@ def signup(request):
             form.save()
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
-<<<<<<< HEAD
-=======
-            #user = User(name=username, password=password)
->>>>>>> 1e528b7bb12039c904c40d5f9d8a2543b9d79912
             user = authenticate(username=username, password=password)
             login(request, user)
             return redirect('index')
@@ -39,20 +31,13 @@ def signup(request):
 
 
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 1e528b7bb12039c904c40d5f9d8a2543b9d79912
 def login(request):
     return redirect('login')
 
 
 
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 1e528b7bb12039c904c40d5f9d8a2543b9d79912
 def index(request):
 
     if request.user.is_authenticated:
@@ -106,24 +91,17 @@ def register(request, event_name):
     if form.is_valid():
         new_register = Registration(
             user=username,
-<<<<<<< HEAD
             key=event, 
-=======
             event=event, 
->>>>>>> 1e528b7bb12039c904c40d5f9d8a2543b9d79912
             date=form.cleaned_data['date'])
         new_register.save()
         event.volunteers = event.volunteers - 1
         event.save()
-<<<<<<< HEAD
         return redirect('profile')
     else:
         raise ValueError('There is an error registering you')
-=======
-    #should return to user profile, with events registered their name
     return redirect('profile')
 
->>>>>>> 1e528b7bb12039c904c40d5f9d8a2543b9d79912
 
 
 
@@ -131,19 +109,13 @@ def register(request, event_name):
 def logout(request):
 
     logout(request.user)
-<<<<<<< HEAD
 
-=======
->>>>>>> 1e528b7bb12039c904c40d5f9d8a2543b9d79912
     return redirect('login')
 
 
 
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 1e528b7bb12039c904c40d5f9d8a2543b9d79912
 def addEvent(request):
 
     if request.user.is_superuser:
@@ -179,59 +151,35 @@ def add(request):
     return redirect('index')
 
 
-<<<<<<< HEAD
 
-def profile(request):
 
-    upcomingEvents = Registration.objects.filter(user=request.user, completed=False) 
-
-=======
 def profile(request):
 
     upcomingEvents = Registration.objects.filter(user=request.user, completed=False)  
->>>>>>> 1e528b7bb12039c904c40d5f9d8a2543b9d79912
     completedEvents = Registration.objects.filter(user=request.user, completed=True)
 
     context = {'completedEvents':completedEvents, 'upcomingEvents':upcomingEvents}
 
     return render(request, 'app1/profile.html', context)
 
-<<<<<<< HEAD
+
 
 
 def delete(request, id):
 
     event = Registration.objects.get(pk=id)
-
-=======
-def delete(request, id):
-
-    event = Registration.objects.get(pk=id)
->>>>>>> 1e528b7bb12039c904c40d5f9d8a2543b9d79912
     event.delete()
 
     return redirect('profile')
     
-<<<<<<< HEAD
-
 
 def deleteEvent(request, id):
-
     event = Event.objects.get(pk=id)
-
-=======
-def deleteEvent(request, id):
-    event = Event.objects.get(pk=id)
->>>>>>> 1e528b7bb12039c904c40d5f9d8a2543b9d79912
     event.delete()
 
     return redirect('index')
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 1e528b7bb12039c904c40d5f9d8a2543b9d79912
 def updateEvent(request, id):
 
     event = Event.objects.get(pk=id)
@@ -262,7 +210,6 @@ def update(request, id):
 
             return redirect('index')
 
-<<<<<<< HEAD
 
 
 def markComplete(request, username):
@@ -280,17 +227,3 @@ def markComplete(request, username):
         else:
             raise ValueError('This is incorrect')
     return render(request, 'app1/index.html')
-=======
-def markComplete(request, id, username):
-    event = Event.objects.get(pk=id)
-    pin = int(event.pin)
-    data = request.POST.copy()
-    pin2 = int(data.get('pin'))
-    user = Registration.objects.get(user=username, event_id=id)
-    if pin == pin2:
-        user.completed = True
-        user.save()
-        return redirect('profile')
-    else:
-        raise ValueError('This is incorrect')
->>>>>>> 1e528b7bb12039c904c40d5f9d8a2543b9d79912
